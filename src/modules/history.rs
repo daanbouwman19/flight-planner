e482bec2ee5627ff7bbbf310d623bf6d00f684ff
Daan Bouwman <daanbouwman19@gmail.com>
2024-11-20 00:03:47 +0100

    added history form

--- src/modules/history.rs
+++ src/modules/history.rs
@@ -3,20 +3,40 @@ use crate::schema::history::dsl::*;
 use diesel::prelude::*;
 use diesel::result::Error;
 
+#[derive(Insertable)]
+#[diesel(table_name = crate::schema::history)]
+struct HistoryForm<'a> {
+    date: String,
+    departure_icao: &'a str,
+    arrival_icao: &'a str,
+    aircraft: i32,
+}
+
+fn create_history<'a>(
+    departure: &'a Airport,
+    arrival: &'a Airport,
+    aircraft_record: &'a Aircraft,
+) -> HistoryForm<'a> {
+    let date_string = chrono::Local::now().format("%Y-%m-%d").to_string();
+
+    HistoryForm {
+        date: date_string,
+        departure_icao: &departure.ICAO,
+        arrival_icao: &arrival.ICAO,
+        aircraft: aircraft_record.id,
+    }
+}
+
 pub fn add_to_history(
     connection: &mut SqliteConnection,
     departure: &Airport,
     arrival: &Airport,
     aircraft_record: &Aircraft,
 ) -> Result<(), Error> {
-    let date_string = chrono::Local::now().format("%Y-%m-%d").to_string();
+    let record = create_history(departure, arrival, aircraft_record);
+
     diesel::insert_into(history)
-        .values((
-            departure_icao.eq(&departure.ICAO),
-            arrival_icao.eq(&arrival.ICAO),
-            aircraft.eq(&aircraft_record.id),
-            date.eq(&date_string),
-        ))
+        .values(&record)
         .execute(connection)?;
 
     Ok(())
