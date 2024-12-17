75fafcd16b7460c84670023ffd188ac990828268
Daan Bouwman <daanbouwman19@gmail.com>
2024-12-17 14:14:53 +0100

    Renamed popup

--- src/gui.rs
+++ src/gui.rs
@@ -435,7 +435,7 @@ impl<'a> Gui<'a> {
                 );
             });
 
-        egui::Window::new("Alert")
+        egui::Window::new("Route")
             .collapsible(false)
             .resizable(false)
             .anchor(egui::Align2::CENTER_CENTER, egui::Vec2::ZERO)
