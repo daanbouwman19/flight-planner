f2268b17a70d7ade8fe8ca19eab870f7e38ef6e7
Daan Bouwman <daanbouwman19@gmail.com>
2025-07-02 21:26:59 +0200

    "cleanup"

--- src/gui/ui.rs
+++ src/gui/ui.rs
@@ -3,9 +3,6 @@ use crate::models::{Aircraft, Airport, History, Runway};
 use crate::modules::routes::RouteGenerator;
 use crate::traits::{AircraftOperations, AirportOperations, HistoryOperations};
 use eframe::egui::{self, TextEdit};
-use eframe::egui_wgpu;
-use eframe::wgpu::util::DeviceExt;
-use eframe::wgpu::{self, RenderPipelineDescriptor};
 use egui::Id;
 use egui_extras::{Column, TableBuilder};
 use rand::prelude::*;
@@ -191,7 +188,6 @@ pub struct Gui<'a> {
     /// State for handling search.
     search_state: SearchState,
     route_generator: RouteGenerator,
-    angle: f32
 }
 
 #[derive(Default)]
@@ -219,123 +215,9 @@ impl RTreeObject for SpatialAirport {
     }
 }
 
-struct RenderResources {
-    pipeline: wgpu::RenderPipeline,
-    bind_group: wgpu::BindGroup,
-    uniform_buffer: wgpu::Buffer,
-}
 
-fn wgpu_setup(cc: &eframe::CreationContext) {
-    let wgpu_render_state = cc.wgpu_render_state.as_ref().unwrap();
-    let device = &wgpu_render_state.device;
-    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-        label: Some("custom3d"),
-        source: wgpu::ShaderSource::Wgsl(include_str!("./shader.wgsl").into()),
-    });
-    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-        label: Some("custom3d"),
-        entries: &[wgpu::BindGroupLayoutEntry {
-            binding: 0,
-            visibility: wgpu::ShaderStages::VERTEX,
-            ty: wgpu::BindingType::Buffer {
-                ty: wgpu::BufferBindingType::Uniform,
-                has_dynamic_offset: false,
-                min_binding_size: None,
-            },
-            count: None,
-        }],
-    });
 
-    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        label: Some("custom3d"),
-        bind_group_layouts: &[&bind_group_layout],
-        push_constant_ranges: &[],
-    });
 
-    let pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
-        label: Some("custom3d"),
-        layout: Some(&pipeline_layout),
-        vertex: wgpu::VertexState {
-            module: &shader,
-            entry_point: None,
-            buffers: &[],
-            compilation_options: wgpu::PipelineCompilationOptions::default(),
-        },
-        fragment: Some(wgpu::FragmentState {
-            module: &shader,
-            entry_point: Some("fs_main"),
-            targets: &[Some(wgpu_render_state.target_format.into())],
-            compilation_options: wgpu::PipelineCompilationOptions::default(),
-        }),
-        primitive: wgpu::PrimitiveState::default(),
-        depth_stencil: None,
-        multisample: wgpu::MultisampleState::default(),
-        multiview: None,
-        cache: None,
-    });
-
-    let uniform_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: Some("custom3d"),
-        contents: bytemuck::cast_slice(&[0.0f32; 4]),
-        usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::UNIFORM,
-    });
-
-    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-        label: Some("custom3d"),
-        layout: &bind_group_layout,
-        entries: &[wgpu::BindGroupEntry {
-            binding: 0,
-            resource: uniform_buffer.as_entire_binding(),
-        }],
-    });
-
-    wgpu_render_state
-        .renderer
-        .write()
-        .callback_resources
-        .insert(RenderResources {
-            pipeline,
-            bind_group,
-            uniform_buffer,
-        });
-}
-
-struct CustomCallback {
-    angle: f32,
-}
-
-impl egui_wgpu::CallbackTrait for CustomCallback {
-    fn prepare(
-            &self,
-            _device: &wgpu::Device,
-            queue: &wgpu::Queue,
-            _screen_descriptor: &egui_wgpu::ScreenDescriptor,
-            _egui_encoder: &mut wgpu::CommandEncoder,
-            callback_resources: &mut egui_wgpu::CallbackResources,
-        ) -> Vec<wgpu::CommandBuffer> {
-      let resources: &RenderResources = callback_resources
-            .get::<RenderResources>()
-            .expect("Failed to get render resources");
-
-        queue.write_buffer(&resources.uniform_buffer, 0, bytemuck::cast_slice(&[self.angle, 0.0, 0.0, 0.0]));
-        Vec::new()
-    }
-
-    fn paint(
-        &self,
-        _info: egui::PaintCallbackInfo,
-        render_pass: &mut wgpu::RenderPass<'static>,
-        callback_resources: &egui_wgpu::CallbackResources,
-    ) {
-        let resources = callback_resources
-            .get::<RenderResources>()
-            .expect("Failed to get render resources");
-        
-        render_pass.set_pipeline(&resources.pipeline);
-        render_pass.set_bind_group(0, &resources.bind_group, &[]);
-        render_pass.draw(0..3, 0..1);
-    }
-}
 
 impl<'a> Gui<'a> {
     /// Creates a new GUI instance.
@@ -381,8 +263,6 @@ impl<'a> Gui<'a> {
             spatial_airports,
         };
 
-        wgpu_setup(cc);
-
         Gui {
             database_pool,
             displayed_items: Vec::new(),
@@ -390,7 +270,6 @@ impl<'a> Gui<'a> {
             popup_state: PopupState::default(),
             search_state: SearchState::default(),
             route_generator,
-            angle: 0.0,
         }
     }
 
@@ -633,9 +512,7 @@ impl<'a> Gui<'a> {
                 route.aircraft.manufacturer, route.aircraft.variant
             ));
 
-            egui::Frame::canvas(ui.style()).show(ui, |ui| {
-                self.render_triangle(ui);
-            });
+            
 
             ui.separator();
             ui.horizontal(|ui| {
@@ -649,11 +526,7 @@ impl<'a> Gui<'a> {
         });
     }
 
-    fn render_triangle(&mut self, ui: &mut egui::Ui) {
-        let (rect, response) =  ui.allocate_exact_size(egui::Vec2::splat(300.0), egui::Sense::drag());
-        self.angle += response.drag_motion().x * 0.01;
-        ui.painter().add(egui_wgpu::Callback::new_paint_callback(rect, CustomCallback {angle: self.angle}));
-    }
+    
 
     /// Handles the action when the "Mark as flown" button is pressed.
     ///
